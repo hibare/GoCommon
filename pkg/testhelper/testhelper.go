fc4ff7dd5515f760a6b7b01165e3b7e16b1e1a47
Shubham Hibare <shubham@hibare.in>
2023-08-18 21:33:21 +0530

    Move createfile to testhelper

--- pkg/testhelper/testhelper.go
+++ pkg/testhelper/testhelper.go
@@ -1,5 +1,31 @@
 package testhelper
 
+import (
+	"os"
+	"path/filepath"
+)
+
 const (
 	TestDataDir = "../testhelper/test_data"
 )
+
+func CreateTestFile() ([]byte, string, error) {
+	file, err := os.CreateTemp("", "test-file-*.txt")
+	if err != nil {
+		return []byte{}, "", err
+	}
+	defer file.Close()
+
+	content := []byte("This is a test file.\nIt contains some sample content.")
+	_, err = file.Write(content)
+	if err != nil {
+		return []byte{}, "", err
+	}
+
+	absPath, err := filepath.Abs(file.Name())
+	if err != nil {
+		return []byte{}, "", err
+	}
+
+	return content, absPath, err
+}
