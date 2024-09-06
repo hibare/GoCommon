a08ba30086e291cc762f087959cb7b9252556020
Shubham Hibare <20609766+hibare@users.noreply.github.com>
2024-09-07 02:20:18 +0530

    feat(context): Add context utils (#104)

---
+++ pkg/context/context.go
@@ -0,0 +1,43 @@
+package context
+
+import (
+	"context"
+)
+
+type ContextKey string
+
+type Context struct {
+	Context context.Context
+}
+
+func NewContext() *Context {
+	return &Context{
+		Context: context.Background(),
+	}
+}
+
+func (c *Context) WithValue(key, value interface{}) *Context {
+	c.Context = context.WithValue(c.Context, key, value)
+	return c
+}
+
+func (c *Context) WithRequestID(requestID string) *Context {
+	return c.WithValue("request_id", requestID)
+}
+
+func (c *Context) WithContext(ctx context.Context) *Context {
+	c.Context = ctx
+	return c
+}
+
+func (c *Context) Get(key interface{}) interface{} {
+	return c.Context.Value(key)
+}
+
+func (c *Context) GetContext() context.Context {
+	return c.Context
+}
+
+func (c *Context) GetRequestID() string {
+	return c.Context.Value("request_id").(string)
+}
