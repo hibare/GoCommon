d6eccc23b7361acd71ef53e3111cbc636da75ba1
Shubham Hibare <20609766+hibare@users.noreply.github.com>
2024-10-22 22:47:09 +0530

    feat(slice): Add slice functions (#109)

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -9,3 +9,62 @@ func SliceContains[T comparable](a T, list []T) bool {
 	}
 	return false
 }
+
+// SliceUnique returns a slice with unique elements
+func SliceUnique[T comparable](list []T) []T {
+	keys := make(map[T]bool)
+	listUnique := []T{}
+	for _, entry := range list {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			listUnique = append(listUnique, entry)
+		}
+	}
+	return listUnique
+}
+
+// SliceDiff returns the difference between two slices
+func SliceDiff[T comparable](a, b []T) []T {
+	mb := make(map[T]bool)
+	for _, x := range b {
+		mb[x] = true
+	}
+	var diff []T
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
+}
+
+// SliceIntersect returns the intersection between two slices
+func SliceIntersect[T comparable](a, b []T) []T {
+	mb := make(map[T]bool)
+	for _, x := range b {
+		mb[x] = true
+	}
+	var intersect []T
+	for _, x := range a {
+		if _, found := mb[x]; found {
+			intersect = append(intersect, x)
+		}
+	}
+	return intersect
+}
+
+// SliceUnion returns the union between two slices
+func SliceUnion[T comparable](a, b []T) []T {
+	ma := make(map[T]bool)
+	for _, x := range a {
+		ma[x] = true
+	}
+	for _, x := range b {
+		ma[x] = true
+	}
+	var union []T
+	for x := range ma {
+		union = append(union, x)
+	}
+	return union
+}
--- pkg/slice/slice_test.go
+++ pkg/slice/slice_test.go
@@ -14,3 +14,39 @@ func TestStringInSlice(t *testing.T) {
 	assert.True(t, SliceContains(3.3, []float64{1.1, 2.2, 3.3}))
 	assert.False(t, SliceContains(33.3, []float64{1.1, 2.2, 3.3}))
 }
+
+func TestSliceUnique(t *testing.T) {
+	assert.ElementsMatch(t, []string{"1", "2", "3"}, SliceUnique([]string{"1", "2", "3", "1", "2"}))
+	assert.ElementsMatch(t, []int{1, 2, 3}, SliceUnique([]int{1, 2, 3, 1, 2}))
+	assert.ElementsMatch(t, []float64{1.1, 2.2, 3.3}, SliceUnique([]float64{1.1, 2.2, 3.3, 1.1, 2.2}))
+	assert.ElementsMatch(t, []string{}, SliceUnique([]string{}))
+	assert.ElementsMatch(t, []int{1}, SliceUnique([]int{1, 1, 1, 1}))
+}
+
+func TestSliceDiff(t *testing.T) {
+	assert.ElementsMatch(t, []string{"1"}, SliceDiff([]string{"1", "2", "3"}, []string{"2", "3"}))
+	assert.ElementsMatch(t, []int{1}, SliceDiff([]int{1, 2, 3}, []int{2, 3}))
+	assert.ElementsMatch(t, []float64{1.1}, SliceDiff([]float64{1.1, 2.2, 3.3}, []float64{2.2, 3.3}))
+	assert.ElementsMatch(t, []string{"1", "2", "3"}, SliceDiff([]string{"1", "2", "3"}, []string{}))
+	assert.ElementsMatch(t, []int{}, SliceDiff([]int{1, 2, 3}, []int{1, 2, 3}))
+	assert.ElementsMatch(t, []int{}, SliceDiff([]int{}, []int{1, 2, 3}))
+}
+
+func TestSliceIntersect(t *testing.T) {
+	assert.ElementsMatch(t, []string{"2", "3"}, SliceIntersect([]string{"1", "2", "3"}, []string{"2", "3", "4"}))
+	assert.ElementsMatch(t, []int{2, 3}, SliceIntersect([]int{1, 2, 3}, []int{2, 3, 4}))
+	assert.ElementsMatch(t, []float64{2.2, 3.3}, SliceIntersect([]float64{1.1, 2.2, 3.3}, []float64{2.2, 3.3, 4.4}))
+	assert.ElementsMatch(t, []string{}, SliceIntersect([]string{"1", "2", "3"}, []string{"4", "5", "6"}))
+	assert.ElementsMatch(t, []int{}, SliceIntersect([]int{1, 2, 3}, []int{}))
+	assert.ElementsMatch(t, []int{}, SliceIntersect([]int{}, []int{1, 2, 3}))
+}
+
+func TestSliceUnion(t *testing.T) {
+	assert.ElementsMatch(t, []string{"1", "2", "3", "4"}, SliceUnion([]string{"1", "2", "3"}, []string{"3", "4"}))
+	assert.ElementsMatch(t, []int{1, 2, 3, 4}, SliceUnion([]int{1, 2, 3}, []int{3, 4}))
+	assert.ElementsMatch(t, []float64{1.1, 2.2, 3.3, 4.4}, SliceUnion([]float64{1.1, 2.2, 3.3}, []float64{3.3, 4.4}))
+	assert.ElementsMatch(t, []string{"1", "2", "3"}, SliceUnion([]string{"1", "2", "3"}, []string{}))
+	assert.ElementsMatch(t, []int{1, 2, 3}, SliceUnion([]int{1, 2, 3}, []int{}))
+	assert.ElementsMatch(t, []int{1, 2, 3}, SliceUnion([]int{}, []int{1, 2, 3}))
+	assert.ElementsMatch(t, []string{}, SliceUnion([]string{}, []string{}))
+}
